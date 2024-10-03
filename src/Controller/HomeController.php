6300fd1b207e6879053c59e434e3d84cf360411e
William Lin <linwilliam14@gmail.com>
2024-10-03 16:22:58 +0200

    Version 4

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController 
+{
+    #[Route('/')]
+    public function accueil(): Response
+    {
+        $accueil = [
+          'name'=>'Agence Web',
+          'description' =>'Voyager comme vous etes',
+          'valeur' => 'Transparent , Sécurité , Accompagnement'
+        ];
+        return $this->render('base.html.twig', [
+
+        ]);
+    }
+
+    #[Route('/destination')]
+    public function destination(): Response
+    {
+        $destination = [
+            'name' => 'Paris',
+            'description' => "C'est beau",
+        ];
+
+        return $this->render('destination.html.twig', [
+            'destination' => $destination,
+        ]);
+    }
+    #[Route('/base')]
+    public function base(): Response
+    {
+        $destination = [
+            'name' => 'Paris',
+            'description' => "C'est beau",
+        ];
+
+        return $this->render('base.html.twig', [
+            'destination' => $destination,
+        ]);
+    }
+}
